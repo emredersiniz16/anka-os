d210a690d9e0cdab9a3c11501e862495a002c9fd
emredersiniz16 <156595390+emredersiniz16@users.noreply.github.com>
2026-07-11 11:21:20 +0300

    Update fly_engine.c

--- core/fly_engine.c
+++ core/fly_engine.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
-// Sinek durumları (Terminal yazısı yok, sadece durum kodu var)
+// Sinek durumları (Terminal yazısı yok, şov yok, sadece durum kodu var. Çekmece korkusu devrede.)
 typedef enum {
-    FLY_IDLE,    // Boşta uçuş
-    FLY_THINK,   // El yüz silme / Düşünme
-    FLY_WAIT     // Komut bekleme
+    FLY_IDLE,    // Boşta uçuş (Standart pusu, sessizce ortamı dinler)
+    FLY_THINK,   // El yüz silme / Düşünme (Arka planda veri işlerken)
+    FLY_WAIT,    // Komut bekleme (Tohumun ekranda dalgalanması)
+    FLY_GHOST,   // CAM DURUMU: İşlemci tüketimi 0, sistemde tamamen saydam ve ağırlıksız.
+    FLY_MIRROR   // AYNA DURUMU: Ortamı kopyala, kullanıcının niyetini yansıt. (Tetiklenme)
 } FlyState;
 
 FlyState current_state = FLY_IDLE;
@@ -12,13 +14,19 @@ FlyState current_state = FLY_IDLE;
 void update_fly_behavior() {
     switch(current_state) {
         case FLY_IDLE:
-            // Sinek ekranda süzülüyor (Random hareketler)
+            // Sinek ekranda görünmez şekilde süzülüyor (Sessiz güç, pusu modu)
             break;
         case FLY_THINK:
-            // Sinek kondu ve elini yüzünü siliyor (Animasyon tetikleyici)
+            // Sinek kondu ve elini yüzünü siliyor (Bilinçaltı animasyon tetikleyicisi)
             break;
         case FLY_WAIT:
-            // Sinek butona yakın bekliyor
+            // Sinek butona (tohuma) yakın bekliyor (İlk damlayı bekliyor)
+            break;
+        case FLY_GHOST:
+            // Tüm arayüz buharlaşır, RAM tüketimi sıfırlanır, sistem cam gibi saydamlaşır.
+            break;
+        case FLY_MIRROR:
+            // Karşıdaki frekansı kopyala, niyet neyse ona dönüş. Soru sorma, yansıt.
             break;
     }
 }
